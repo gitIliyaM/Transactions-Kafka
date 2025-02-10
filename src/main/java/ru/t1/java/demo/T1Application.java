8dfb80e5ab102a734eeaf11fc55514465e7d1735
WiseAdviceIM <wiseadviceim@roadmapschool.ru>
2025-02-10 12:58:50 +0300

    finished

---
+++ src/main/java/ru/t1/java/demo/T1Application.java
@@ -0,0 +1,13 @@
+package ru.t1.java.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(T1Application.class, args);
+    }
+
+}
